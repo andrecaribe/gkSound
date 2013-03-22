bc2cbffc6096e8eacca638a514bac37583486c12
André Caribé <decocaribe@gmail.com>
2013-03-21 22:00:54 -0300

    Lot of changes to improve event handlers. Now have some documentation. More functions to work more easy.

--- README.md
+++ README.md
@@ -8,32 +8,76 @@ Simple HTML5 sound manager in JavaScript.
 
 Sounds should be .mp3 and .ogg to work correctly on browsers.
 
+    <link rel="stylesheet" type="text/css" href="styles/gkSound-default.css">
     <script type="text/javascript" src="gkSound.js"></script>
     <script type="text/javascript">
       gkSound.init();
-      gkSound.addSound('id','path/to/sound', false, false, function() {
-        console.log('Loaded!');
+      gkSound.addSound('id','path/to/sound', false, false, function(status) {
+        console.log(status.message);
       });
       gkSound.playSound('id');
     </script>
-  
+    
+
 ###Commands
+
 #####gkSound.init();
-#####gkSound.addSound('id','path/to/sound', isTrack, autoPlay, onLoaded);
+Need to use other funcitions properly. Must be called before all other gkSound functions.
+This function create a default mute/unmute button and a hidden conteiner to storage sound tags.
+
+#####gkSound.addSound('id','path/to/sound', isTrack, autoPlay, callback);
+Simple away to add sound in your page. Supports MP3 and OGG files.
+You need to pass some parameters: id, path/to/file, isTrack, autoPlay and callback function.
+If you dont pass file extension on "path/to/sound", gkSound chooses MP3 - if browser support it - or OGG - if not.
+Callbacks return an Object like { message:"Sound loaded.", code:0 }. These codes are according HTML5 specifications describers here.
+
 #####gkSound.removeSound('id');
+Remove sound from the page.
+If sound not exists return an erro code.
+
 #####gkSound.playSound('id', override, onComplete);
+Play any sound with id passed in parameters.
+Override sets the sound to replay even before its completion, very util to sound effects.
+onComplete trigger an event when sound is played completely.
+
 #####gkSound.pauseSound('id');
+Pauses a specific sound.
+
+#####gkSound.pauseAllSounds();
+Pauses all music tracks and sound.
+Util if you want to pause your game.
+
 #####gkSound.stopSound('id');
+Stops a specific sound.
+
 #####gkSound.stopAllSounds();
+Stops all music tracks and sound.
+
 #####gkSound.playTrack();
+Plays an music track.
+
 #####gkSound.stopTrack();
+Stops an music track.
+
 #####gkSound.toggleSounds();
+Turn on/off all sounds controlled by gkSound.
+
+#####gkSound.hasMP3Support();
+Verify if client browser supports MP3 (audio/mpeg).
+
+#####gkSound.hasOGGSupport();
+Verify if client browser supports OGG (audio/ogg).
+
 
 ###Roadmap
 
-- [ ] implements removeSound method
+- [X] implements removeSound method
 - [ ] implements nextTrack and prevTrack methods
 - [ ] implements callback on track is complete
 - [ ] implements autoVolume
 - [ ] implements autoPan
-- [ ] move away CSS from standard button to a new document.
+- [X] move away CSS from standard button to a new document.
+- [ ] documentation code
+- [ ] improve example
+- [ ] more examples
+- [ ] minified version
--- gkSound.js
+++ gkSound.js
@@ -4,19 +4,21 @@
  var gkSound = (function() {
     
     var soundList = [];
+    var musicList = [];
+
     var currentTrack = null;
     var conteiner;
     var muteButton;
     var isActive;
-    var textOn  = 'On';
-    var textOff = 'Off';
+    var textOn  = '<div class="btnImage" style="left:0"></div>';
+    var textOff = '<div class="btnImage" style="left:-100%"></div>';
 
     var createConteiner = function() {
         document.body.innerHTML += '<div id="sounds" style="display:none"></div>';
     }
 
     var createMuteButton = function() {
-    	document.body.innerHTML = '<div id="btnMute" style="cursor:pointer; width:30px; padding:15px 5px 0 5px; font-size:16px; text-align:center; height:25px; background: #000; opacity:0.8; color:#fff; position:absolute; z-index:1000"></div>' + document.body.innerHTML;
+    	document.body.innerHTML = '<div id="btnMute"><div class="btnImage"></div></div>' + document.body.innerHTML;
         muteButton = 'btnMute';
     }
 
@@ -40,10 +42,48 @@
         for (var i = soundList.length - 1; i >= 0; i--) {
             if( soundList[i].id == id) {
                 soundExists = true;
+                break;
+            }
+        }
+
+        for (var i = musicList.length - 1; i >= 0; i--) {
+            if( musicList[i].id == id) {
+                soundExists = true;
+                break;
             }
         };
 
         return soundExists;
+    }
+
+    var getFileURL = function(file) {
+        var url;
+        var query;
+        var type;
+        
+        // Select type of file by browser support
+        type = gkSound.hasMP3Support() == true ? '.mp3' : '.ogg';
+        
+        // Select type of file by file name
+       
+        if( file.search(".mp3") != -1 ){
+            query = file.split('.mp3');
+            type = '.mp3';
+        } else if( file.search(".ogg") != -1 ){
+            query = file.split('.ogg');
+            type = '.ogg';
+        } else {
+            query = null;
+        }
+
+        // Mount
+        if(query != null) {
+            url = query[0]+type+(query[1] ? query[1] : "");
+        } else {
+            url = file+type;
+        }
+
+        return url;
     }
 
     return {
@@ -75,72 +115,95 @@
         },
 
     	addSound: function (id, file, isTrack, autoPlay, callback) {
-    		
-            var addHTML = function(){
-                var content = '';
-                var query = file.split('.mp3');
-
-                content+= isTrack == true ? '<audio id="'+id+'" loop >' : '<audio id="'+id+'">';
-                        content+= '<source src="'+query[0]+'.ogg'+(query[1] ? query[1] : "")+'" type="audio/ogg" />';
-                        content+= '<source src="'+query[0]+'.mp3'+(query[1] ? query[1] : "")+'" type="audio/mpeg" />';
-                content+= '</audio>';
-
-                document.getElementById("sounds").innerHTML += content;
-            }
 
             var hasAutoPlay = function(){
                 if( autoPlay == true ){
                     if( isTrack == false ) {
-                        gkSound.playSound(id);
+                        gkSound.playTrack();
                     } else {
                         gkSound.playTrack();
                     }
                 }
             }
 
-            var hasCallback = function(){
+            var hasCallback = function(status){
                 if( callback != undefined ){
-                    callback();
+                    callback(status);
                 }
             }
 
-            var canPLay = function(){
-                var soundElement = document.getElementById(id);
-                var listener = soundElement.addEventListener("canplaythrough", function() {
-                    
-                    soundElement.removeEventListener("canplaythrough", listener);
-
-                    hasAutoPlay();
-                    hasCallback();
-                    
-                });
-            }
-
             if( !hasSound(id) ) {
-                
-                addHTML();
 
-                soundList.push( { "id":id, "file":file, "isTrack":isTrack} );
+                var audio = new Audio( getFileURL(file) );
+                    audio.setAttribute("id", id);
 
-                canPLay();
+                if(isTrack) {
+                    audio.setAttribute("loop", "loop");
+                    musicList.push(audio);
+                } else {
+                    soundList.push(audio);
+                }
                 
+                document.getElementById('sounds').appendChild(audio);
+
+                audio.addEventListener('canplaythrough', function() {
+                    hasAutoPlay();
+                    hasCallback( { message:"Sound loaded.", code:0 } );
+                }, false);
+                
+                audio.addEventListener('error', function() {
+                    gkSound.removeSound(id);
+                    hasCallback( { message:"Error! For more information see http://www.w3.org/html/wg/drafts/html/master/embedded-content-0.html#dom-media-error", code:audio.error.code } );
+                }, false);
             } else {
-                hasAutoPlay();
-                hasCallback();
+                hasCallback( { message:"Sound already exists.", code:-1 } );
             }
     		
     	},
 
         removeSound: function(id){
-            element.parentNode.removeChild(element);
-            // TODO: remover do vetor!
+            
+            var index = 0;
+            var audio = undefined;
+
+            while( audio == undefined || index < soundList.length ){
+                if( soundList[index].id == id ) {
+                    audio = soundList[index];
+                    soundList.splice(index,1);
+                }
+                index++;
+            }
+
+            index = 0;
+
+            while( audio == undefined || index < musicList.length ){
+                if( musicList[index].id == id ) {
+                    audio = musicList[index];
+                    musicList.splice(index,1);
+                }
+                index++;
+            }
+
+            if( audio == undefined ){
+                //throw "Error! Sound not exists.";
+                return { message:"Sound not exists.", code:-1 };
+            }
+            
+            audio.parentNode.removeChild(audio);
+
+            return { message:"Sound removed.", code:0 };
         },
 
     	playSound: function(id, override, onComplete) {
             var element = document.getElementById(id);
 
-            var hasCallback = function(){
-                if( onComplete != undefined ){ onComplete(); }
+            var hasCallback = function(status){
+                if( onComplete != undefined ){ onComplete(status); }
+            }
+
+            var onEnded = function() {
+                element.removeEventListener('ended', onEnded);
+                hasCallback( { message:"Sound played.", code:0 } );
             }
 
             if( isActive ) {
@@ -156,16 +219,27 @@
                     element.play();
                 }
 
-                // On complete
-                setTimeout(function(){ hasCallback(); }, 1000 * (element.duration));
+                element.addEventListener('ended', onEnded, false);
 
             } else {
-               hasCallback();
+               hasCallback( { message:"Sound was muted.", code:-1 } );
             }
     	},
 
         pauseSound: function(id) {
             document.getElementById(id).pause();
+        },
+
+        pauseAllSounds: function() {
+            // Music
+            if(currentTrack != null) {
+                gkSound.pauseSound(currentTrack);
+            }
+            
+            // Sounds
+            for (var i = soundList.length - 1; i >= 0; i--) {
+                gkSound.pauseSound(soundList[i].id);
+            }
         },
 
     	stopSound: function(id) {
@@ -180,21 +254,24 @@
     	},
 
     	stopAllSounds: function() {
-    		currentTrack = null;
-
+            // Music
+            if(currentTrack != null) {
+                gkSound.stopSound(currentTrack);
+                currentTrack = null;
+            }
+            
+            // Sounds
     		for (var i = soundList.length - 1; i >= 0; i--) {
                 gkSound.stopSound(soundList[i].id);
-    		};
+    		}
     	},
 
     	playTrack: function() {
             if ( isActive ){
-        		for (var i = soundList.length - 1; i >= 0; i--) {
-        			if(soundList[i].isTrack) {
-                        document.getElementById(soundList[i].id).play();
-                        currentTrack = soundList[i].id;
-        				break;
-        			}
+        		for (var i = musicList.length - 1; i >= 0; i--) {
+        			document.getElementById(musicList[i].id).play();
+                    currentTrack = musicList[i].id;
+                    break;
         		}
             }
     	},
